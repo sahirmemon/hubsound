b93eb39591d22e1eff94765619dd29b780aa6597
Sahir Memon <sahir.memon@gmail.com>
2016-06-17 12:54:08 -0600

    Initial commit

---
+++ src/index.js
@@ -0,0 +1,24 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import configureStore from './stores/configureStore';
+import * as actions from './actions';
+import Stream from ‘./components/Stream';
+
+const tracks = [
+  {
+    title: 'Some track'
+  },
+  {
+    title: 'Some other track'
+  }
+];
+
+const store = configureStore();
+store.dispatch(actions.setTracks(tracks));
+
+ReactDOM.render(
+  <Stream />,
+  document.getElementById('app')
+);
+
+module.hot.accept();
